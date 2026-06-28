5a223378ff837da593375668912dbac99bedf325
TheHighestTimeline <291643968+TheHighestTimeline@users.noreply.github.com>
2026-06-28 15:40:06 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['libsodium-wrappers'],
+  },
+  webpack: (config) => {
+    config.externals = config.externals || [];
+    config.externals.push({ 'libsodium-wrappers': 'commonjs libsodium-wrappers' });
+    return config;
+  },
 }
 
 module.exports = nextConfig
